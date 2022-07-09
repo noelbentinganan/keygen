dd5bf359611d018e77b0af27c364d81899a0308e
John Noel Bentinganan <jnoelbentinganan@gmail.com>
2022-07-09 19:02:32 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [serial0, setSerial0] = useState();
+  const [serial1, setSerial1] = useState();
+  const [serial2, setSerial2] = useState();
+  const [serial3, setSerial3] = useState();
+
+  const handleClick = () => {
+    setSerial0(Math.floor(1000 + Math.random() * 9000));
+    setSerial1(Math.floor(1000 + Math.random() * 9000));
+    setSerial2(Math.floor(1000 + Math.random() * 9000));
+    setSerial3(Math.floor(1000 + Math.random() * 9000));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>S K I D R O W</h1>
+      <hr />
+      <p>Universal Key Generator. Click "Generate" button to get your serial keys!</p>
+      <p className='serial'>{ serial0 } - { serial1 } - { serial2 } - { serial3 }</p>
+      <button onClick={ handleClick }>Generate</button>
     </div>
   );
 }
